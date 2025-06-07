60450411f0762e7f7d6284f5839c51448cd06767
0nepeop1e <engshun.lim@gmail.com>
2025-06-08 02:11:49 +0800

    shortern escape

--- src/escape.ts
+++ src/escape.ts
@@ -1,12 +1,13 @@
-const html_danger = /[&<>"']/g;
-const html_replace = {
-	'&': '&amp;',
-	'<': '&lt;',
-	'>': '&gt;',
-	'"': '&quot;',
-	"'": '&#39;'
-} as Record<string, string>;
-
 export function e(str: string): string {
-	return str.replace(html_danger, (match) => html_replace[match]!);
+	return str.replace(
+		/[&<>"']/g,
+		(match) =>
+			({
+				'&': '&amp;',
+				'<': '&lt;',
+				'>': '&gt;',
+				'"': '&quot;',
+				"'": '&#39;'
+			})[match]!
+	);
 }
